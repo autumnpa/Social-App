d1a283bed30ca4b212b69e9ed5243a4cf368a18d
Autumn Parker <agmparker@gmail.com>
2023-03-16 10:57:29 -0600

    Document Organization Changes
    - Updated directory naming conventions
    - Created new folders and moved files
    - Trying to stay as organized as I can from the beginning to avoid any mistakes or getting lost in my directories

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
-// Home Screen holds components like:
-// Header
-// Images
-// Footer
 const HomeScreen = () => {
   return <SafeAreaView>{/* Create header in here */}</SafeAreaView>;
 };
