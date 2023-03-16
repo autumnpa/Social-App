6789949a3d8b4407fa0372df401098fa1049c537
Autumn Parker <agmparker@gmail.com>
2023-03-16 16:56:28 -0600

    Logo Resize
    - Created a stylesheet and added styling to the logo

--- components/home/AppHeader.js
+++ components/home/AppHeader.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 const AppHeader = () => {
   return (
     <View>
-      <Image source={require('../../assets/logo.png')} />
+      <Image style={styles.logo} source={require('../../assets/logo.png')} />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: 'contain',
+  },
+});
 export default AppHeader;
