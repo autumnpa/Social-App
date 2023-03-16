103ff8d033a6bfbef309730e3d1ac3d442cace32
Autumn Parker <agmparker@gmail.com>
2023-03-16 17:28:54 -0600

    Icon Section Planning
    - Getting the container and styling set up for the views within the header area

--- components/home/AppHeader.js
+++ components/home/AppHeader.js
@@ -4,13 +4,27 @@ import {View, Text, Image, StyleSheet} from 'react-native';
 // Header needs to be a container with 2 views (divs) inside it to hold the logo and the icons
 const AppHeader = () => {
   return (
-    <View>
+    // Logo container (left div)
+    <View style={styles.logoContainer}>
       <Image style={styles.logo} source={require('../../assets/logo.png')} />
+      {/* Icon container (right div) */}
+      <View style={styles.iconContainer} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  logoContainer: {
+    // Spaces out the items
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // Column is default so need to specify row so thigns align properly
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+  },
   logo: {
     width: 100,
     height: 50,
