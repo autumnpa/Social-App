981bb162d9d3358bab666a376bb1293a7cdca522
Autumn Parker <agmparker@gmail.com>
2023-03-16 10:49:03 -0600

    File Plans
    - Added some comments for a basic start to my plan for the home screen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
+// Home Screen holds components like:
+// Header
+// Images
+// Footer
 const HomeScreen = () => {
   return (
     <View>
