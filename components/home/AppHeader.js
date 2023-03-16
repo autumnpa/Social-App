5a7f6f1a1572c3580180c0c4646878653c775e80
Autumn Parker <agmparker@gmail.com>
2023-03-16 16:44:28 -0600

    Prep to Add Logo

--- components/home/AppHeader.js
+++ components/home/AppHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 const AppHeader = () => {
   return (
     <View>
-      <Text style={{color: 'white'}}>Hello</Text>
+      <Image source={require('')} />
     </View>
   );
 };
