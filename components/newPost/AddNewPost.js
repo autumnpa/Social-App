bb808e8d8e3ab095fb0a6b65e92f012134c6347c
Autumn Parker <agmparker@gmail.com>
2023-03-17 16:18:04 -0600

    New Screen Files
    - Creating more files for new screen and the components it needs

---
+++ components/newPost/AddNewPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const NewPostScreen = () => {
+  return (
+    <View>
+      <Text>Add New Post</Text>
+    </View>
+  );
+};
+
+export default NewPostScreen;
--- screens/NewPostScreen.js
+++ screens/NewPostScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import AddNewPost from '../components/newPost/AddNewPost';
 
 const NewPostScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>New Post Screen!</Text>
+    <SafeAreaView style={styles.container}>
+      <AddNewPost />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default NewPostScreen;
