a50e6b19c9f2d6c0741120764eecef29c80102bd
Autumn Parker <agmparker@gmail.com>
2023-03-16 19:48:05 -0600

    Data Loop
    - Imported data from an external file I created
    - Set up a way for the array to be looped through

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
+import {users} from '../../userdata/users';
 
 const Stories = () => {
   return (
     <View>
+      {users.map((story, index) => (
+        <Image source={{uri: story.image}} />
+      ))}
       <Text>Stories</Text>
     </View>
   );
