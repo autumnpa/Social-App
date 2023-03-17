2613ac654705072bf15402414d410df13b116142
Autumn Parker <agmparker@gmail.com>
2023-03-17 16:12:59 -0600

    New Screen Creation
    - Got the base for a new screen that will be navigated to from the tabs

---
+++ screens/NewPostScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const NewPostScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>New Post Screen!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default NewPostScreen;
