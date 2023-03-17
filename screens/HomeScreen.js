da0f0ab119c1aa157761ac5ffcc8538bc4ec953c
Autumn Parker <agmparker@gmail.com>
2023-03-16 19:29:44 -0600

    Render Test
    - Testing that my stories component is rendering on my home screen properly

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import {Text, View, SafeAreaView, StyleSheet} from 'react-native';
 import AppHeader from '../components/home/AppHeader';
+import Stories from '../components/home/Stories';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       {/* Add header component here */}
       <AppHeader />
+      <Stories />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'grey',
+    // backgroundColor: 'grey',
     flex: 1,
   },
 });
