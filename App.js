eaf9d13c2e07be29ad688c98f0aa0ba4ec171f0b
Autumn Parker <agmparker@gmail.com>
2023-03-17 16:20:49 -0600

    Screen Switch
    - Commented out home screen render so I can work in my new screen to get it built

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import HomeScreen from './screens/HomeScreen';
+// import HomeScreen from './screens/HomeScreen';
+import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
-  return <HomeScreen />;
+  // return <HomeScreen />;
+  return <NewPostScreen />;
 }
