fcb6efd349464fb28701cba74a4535b781c9beda
Autumn Parker <agmparker@gmail.com>
2023-03-16 20:06:15 -0600

    User Data Updates
    - Updated the images in the user data array
    - Images are rendering (needs some fixes later)
    - Need to style as well

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 import {users} from '../../userdata/users';
 
 const Stories = () => {
   return (
     <View>
       {users.map((story, index) => (
-        <Image source={{uri: story.image}} />
+        <Image source={{uri: story.image}} style={styles.story} />
       ))}
       <Text>Stories</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  story: {
+    width: 80,
+    height: 80,
+  },
+});
 
 export default Stories;
