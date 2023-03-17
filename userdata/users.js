09e477cf3e5f423921aa57115747f46902b1b47e
Autumn Parker <agmparker@gmail.com>
2023-03-16 19:38:05 -0600

    Fake User Data
    - Created some fake user data to use within the app

---
+++ userdata/users.js
@@ -0,0 +1,29 @@
+// Create some user data to display with the stories bar under the header
+// Array with username and an image for a story circle placeholder
+export const users = [
+  {
+    user: 'chililime',
+    // ADD IMAGE URL HERE
+    // image: ''
+  },
+  {
+    user: 'cpost',
+    // ADD IMAGE URL HERE
+    // image: ''
+  },
+  {
+    user: 'porgy',
+    // ADD IMAGE URL HERE
+    // image: ''
+  },
+  {
+    user: 'fuzzz',
+    // ADD IMAGE URL HERE
+    // image: ''
+  },
+  {
+    user: 'marley',
+    // ADD IMAGE URL HERE
+    // image: ''
+  },
+];
