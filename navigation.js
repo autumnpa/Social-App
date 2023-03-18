84de48cc8b8d45c56bb3399f30b828bd3eaac28f
Autumn Parker <agmparker@gmail.com>
2023-03-17 18:14:57 -0600

    Setting Up Screen Stack

--- navigation.js
+++ navigation.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {createStackNavigator} from 'react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
 
-const navigation = () => {
+const Stack = createStackNavigator();
+
+const navigationStack = () => {
   return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Create screen stack */}
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-export default navigation;
+export default navigationStack;
