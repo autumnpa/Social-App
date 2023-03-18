279374230cb0033f93e5fa7d36dde46c3ef38e1b
Autumn Parker <agmparker@gmail.com>
2023-03-17 18:43:31 -0600

    Setting Up stack

--- navigation.js
+++ navigation.js
@@ -3,15 +3,17 @@ import {View, Text} from 'react-native';
 import {createStackNavigator} from 'react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
+import NewPostScreen from './screens/NewPostScreen';
 
 const Stack = createStackNavigator();
 
 const navigationStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="HomeScreen">
         {/* Create screen stack */}
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="NewPostScreen" component={NewPostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
