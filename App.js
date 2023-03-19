1f12e18ef69bb96f2f3358ba3a4339044a53c1bf
Autumn Parker <agmparker@gmail.com>
2023-03-18 19:02:07 -0600

    Completed Tab Navigation
    - Got the tab navigation completed
    - There are no icons because I cannot get the installation to work
    But everything functions !!! :)))))

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
-// import NewPostScreen from './screens/NewPostScreen';
-// import SearchScreen from './screens/SearchScreen';
+import NewPostScreen from './screens/NewPostScreen';
+import SearchScreen from './screens/SearchScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <HomeScreen />;
-  // return <NewPostScreen />;
-  // return <SearchScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="New Post" component={NewPostScreen} />
+        <Tab.Screen name="Reels" component={HomeScreen} />
+        <Tab.Screen name="My Profile" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
