ef8f7d93b95aaf66f3ca8ec59b33eb473db426bc
Autumn Parker <agmparker@gmail.com>
2023-03-18 19:03:24 -0600

    Getting Nav Stack Prepped
    - Preparing the navigation stack for the new post screen ready
    - It will have the initial screen then next to choose something to post then next again to confirm sharting that post

--- screens/NewPostScreen.js
+++ screens/NewPostScreen.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
-import AddNewPost from '../components/newPost/AddNewPost';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AddNewPost from '../components/newPost/AddNewPost';
+
+const Stack = createStackNavigator();
+
+const NewPostStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="AddNewPost" component={AddNewPost} />
+      <Stack.Screen name="ConfirmPost" component={ConfirmPost} />
+    </Stack.Navigator>
+  );
+};
+
 
 const NewPostScreen = () => {
   return (
