f83a48f5ab5a3665bdab5520a43879800862a020
Autumn Parker <agmparker@gmail.com>
2023-03-18 18:29:10 -0600

    The Icons DO NOT Work!
    - I cannot get the tab nav icons to work still
    - I have installed 475834758943756347691 different things relaunched deleted the app reinstalled etc.
    - Nothing works so I am not even attempting it anymore

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 // import NewPostScreen from './screens/NewPostScreen';
 // import SearchScreen from './screens/SearchScreen';
-import Ionic from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return <HomeScreen />;
